cf0dd4cce1aa317908fdd313e0b0341fc29310bb
Justin Chen <justin@setprotocol.com>
2020-11-17 16:54:11 -0500

    Test url

--- src/utils/snapshotApi.ts
+++ src/utils/snapshotApi.ts
@@ -1,10 +1,10 @@
 import axios from 'axios'
 
-const BASE_URL =
-  process.env.REACT_APP_SNAPSHOT_URL || 'https://hub.snapshot.page/api'
+const BASE_URL = process.env.REACT_APP_SNAPSHOT_URL // || 'https://hub.snapshot.page/api'
 const INDEX_PROPOSALS_PATH = 'index/proposals'
 
 export const fetchIndexProposals = () => {
+  console.log(BASE_URL)
   const indexProposalsUrl = `${BASE_URL}/${INDEX_PROPOSALS_PATH}`
 
   return axios
